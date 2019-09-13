35ccf4554e92fbd9d0de17531954508d25961390
Karel Blavka <karel.blavka@hardwario.com>
2019-09-14 00:57:36 +0200

    Update populace

--- question_modules/populace/__init__.py
+++ question_modules/populace/__init__.py
@@ -6,7 +6,7 @@ import sys
 sys.path.append("..")
 import csv
 import os
-from utils import location
+from utils import location, answer_proposer
 
 path = os.path.dirname(__file__)
 
@@ -20,25 +20,20 @@ with open(os.path.join(path, 'DEM07D.csv'), newline='') as csvfile:
     for row in datareader:
         if row[1] in okresy:
             data[row[1]] = {
-                'narozeni': row[2],
-                'kluci': row[3],
-                'holky': row[4],
-                'prvorodicka_prum_vek': row[10]
+                'narozeni': int(row[2]),
+                'kluci': int(row[3]),
+                'holky': int(row[4]),
+                'prvorodicka_prum_vek': float(row[10])
             }
 
 def get_question(okres):
     row = data[okres]
 
-    return {
-        'question': 'Kolik dětí se narodilo v roce 2017?',
-        'answers': [
-            'A: 0 - 10',
-            'B: 10 - 20',
-            'C: 20 - 30',
-            'D: 30 - 40'
-        ],
-        'corrects': 1,
-        'source': 'ČSÚ - Souhrnná data o České republice',
-        'value': row['narozeni'] + ' z toho ' + row['holky'] + ' holčiček a ' + row['kluci'] + ' kluků',
-        'more_html': '<div></div>'
-    }
+    question = answer_proposer.propose_integer_answer(row['narozeni'])
+
+    question['question'] = 'Kolk dětí se narodilo v roce 2017?'
+    question['source'] = 'ČSÚ - Souhrnná data o České republice'
+    question['value'] = '{narozeni} z toho {holky} holčiček a {kluci} kluků'.format(**row)
+    question['more_html'] = '<div></div>'
+
+    return question
