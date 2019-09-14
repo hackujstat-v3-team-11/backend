3c309c0cdf1f5aff75ad2964816c7b3f968b8963
Karel Blavka <karel.blavka@hardwario.com>
2019-09-14 11:14:09 +0200

    Add test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from question_modules import populace, hospodarska_zvirata
+from utils import location
+from pprint import pprint
+import question_modules
+
+# pprint(hospodarska_zvirata.data)
+
+pprint(populace.get_question('Praha'))
+
+# pprint(question_modules.get_questions('Liberec'))
